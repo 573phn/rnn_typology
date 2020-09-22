fe5526e26b4614ca8aac4775b2cf807fd62d230c
573phn <stephan@spor.tel>
2020-09-22 18:33:38 +0200

    Added option to define seed from command line

--- datasets_creation/jobscript_cases.sh
+++ datasets_creation/jobscript_cases.sh
@@ -15,10 +15,11 @@ DATADIR='/data/'"${USER}"'/rnn_typology'
 ERROR=$(cat <<-END
   jobscript.sh: Incorrect usage.
   Correct usage options are:
-  - jobscript.sh [sov|svo|ovs|osv|vso|vos|random|vso60rest8|vso30rest14|vos60rest8|vos30rest14] [na-d na-s na-a] [0|1]
+  - jobscript.sh [sov|svo|ovs|osv|vso|vos|random|vso60rest8|vso30rest14|vos60rest8|vos30rest14] [na-d na-s na-a] [0|1] [random_seed_int]
   (respectively: order case-system print-txt?)
 END
 )
+RE='^[0-9]+$'
 
 # Load Python module
 module load Python/2.7.16-GCCcore-8.3.0
@@ -29,7 +30,8 @@ source "${DATADIR}"/env2/bin/activate
 # Use Rafvogel script to reorder text
 if [[ "$1" =~ ^(sov|svo|ovs|osv|vso|vos|random|vso60rest8|vso30rest14|vos60rest8|vos30rest14)$ ]] \
     && [[ "$2" =~ ^(na-d|na-s|na-a)$ ]] \
-    && [[ "$3" =~ ^(0|1)$ ]]; then
+    && [[ "$3" =~ ^(0|1)$ ]] \
+    && [[ "$4" =~ $RE ]]; then
   python2 main.py --dataset dev \
                   --agreement-marker "$2" \
                   --add-cases 1 \
@@ -37,7 +39,7 @@ if [[ "$1" =~ ^(sov|svo|ovs|osv|vso|vos|random|vso60rest8|vso30rest14|vos60rest8
                   --mark-verb 1 \
                   --lemmatize 1 \
                   --print-txt "$3" \
-                  --random-seed 5
+                  --random-seed "$4"
 
 else
   echo "${ERROR}"
