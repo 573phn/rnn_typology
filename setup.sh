1afba91758d2d19cabfc2be329687b783e839ce8
573phn <stephan@spor.tel>
2020-09-22 18:32:20 +0200

    Added parameter to mkdir command

--- setup.sh
+++ setup.sh
@@ -6,8 +6,8 @@ module load Python/2.7.16-GCCcore-8.3.0
 # Set data location
 DATADIR='/data/'"${USER}"'/rnn_typology'
 
-# Prepare /data directories
-mkdir "${DATADIR}"
+# Prepare /data directories, ignore warning in case it already exists
+mkdir -p "${DATADIR}"
 
 # Create virtual environment
 python2 -m virtualenv "${DATADIR}"/env2
